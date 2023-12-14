b2514124a043d6a00269ba3c59e4100707665ff2
thakurvinaysingh <vinay.thakur5497@gmail.com>
2023-12-14 12:44:09 +0530

    ten-8 comiit

--- app.js
+++ app.js
@@ -5,32 +5,32 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors')
 const session = require('express-session');
-//const MongoDBStore = require('connect-mongodb-session')(session);
+const MongoDBStore = require('connect-mongodb-session')(session);
  
 
 const app = express();
 const PORT = process.env.PORT || 8000
 //----------session--------//
-// const store = new MongoDBStore({
-//     uri: 'mongodb+srv://vinaysingh5497:Roopwati@cluster0.lac7pmr.mongodb.net/?retryWrites=true&w=majority',
-//     collection: 'sessions',
-//   });
-// store.on('error', function (error) {
-//     console.log(error);
-//   });
+const store = new MongoDBStore({
+    uri: 'mongodb+srv://vinaysingh5497:Roopwati@cluster0.lac7pmr.mongodb.net/?retryWrites=true&w=majority',
+    collection: 'sessions',
+  });
+store.on('error', function (error) {
+    console.log(error);
+  });
 
 app.use(session({
     secret: 'your-secret-key', // Replace with a secret key
     resave: false,
     saveUninitialized: true,
-    // store: store,
-    // cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 }, 
+    store: store,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 }, 
   }));
   
 //--middleware----//
 app.use(cors());
-// app.use(express.static('public'));
-app.use('/images',express.static('public/uploads'));
+app.use(express.static('public'));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
